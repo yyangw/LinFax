32186b3defd4f62049b5a227ada15a45d650b649
YiyangVVang <maomaoyu686@gmail.com>
2017-07-28 00:14:15 -0500

    edited configure

--- src/config.h
+++ src/config.h
@@ -32,25 +32,25 @@
 #define HAVE_UNISTD_H 1
 
 /* Name of package */
-#define PACKAGE "hamfax"
+#define PACKAGE "linfax"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "cschmitt@users.sourceforge.net"
+#define PACKAGE_BUGREPORT "tonych@protonmail.ch"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "hamfax"
+#define PACKAGE_NAME "linfax"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "hamfax 0.8.1"
+#define PACKAGE_STRING "linfax 0.1.1"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "hamfax"
+#define PACKAGE_TARNAME "linfax"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.8.1"
+#define PACKAGE_VERSION "0.1.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -68,4 +68,4 @@
 /* #undef USE_PTC */
 
 /* Version number of package */
-#define VERSION "0.8.1"
+#define VERSION "0.1.1"
